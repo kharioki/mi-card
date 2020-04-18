579b61178693dfb00c5174b7ca2323dd9d617c33
kharioki <antolisah@gmail.com>
2020-04-18 13:09:13 +0300

    row and column layouts

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,12 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.star, size: 50),
+              Icon(Icons.star, size: 200),
+              Icon(Icons.star, size: 50),
+            ],
           ),
         ),
       ),
